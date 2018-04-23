0651366548985fa99abc0434629510df8677af54
Oriol Pérez <oriol@fxstreet.com>
2018-04-23 11:56:00 +0200

    Update calendarico.js

--- files-to-copy/calendarico.js
+++ files-to-copy/calendarico.js
@@ -1,4 +1,4 @@
-$("#templates").load("template.html #template1",function(){
+$("#templates").load("files-to-copy/template.html #template1",function(){
           var template = document.getElementById('template1').innerHTML;
           var output = Mustache.render(template, view);
           $("#person").html(output);
