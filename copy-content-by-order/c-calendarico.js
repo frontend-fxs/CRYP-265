f6ec2d89be881da60e9c431984974aaa8804d079
frontend-fxs <oriol@fxstreet.com>
2018-04-23 18:01:13 +0200

    update

--- copy-content-by-order/c-calendarico.js
+++ copy-content-by-order/c-calendarico.js
@@ -1,21 +1,15 @@
-	var dinamicVars = {};
-	var htmlTemplateFile = 'copy-content-by-order/d-mustache.html';
-	var initDinamicVars = function() {
-		dinamicVars.Translations = translations;
-	};
+var dinamicVars = {};
+var htmlTemplateFile = 'copy-content-by-order/d-mustache.html';
+var initDinamicVars = function() {
+	dinamicVars.Translations = translations;
+};
 
-	var htmlRender = function() {
-		initDinamicVars();
-		if (FXStreet) {
-			FXStreet.Util.loadHtmlTemplate(htmlTemplateFile).done(function(template) {
-				var rendered = FXStreet.Util.renderByHtmlTemplate(template, dinamicVars);
-				$('#icoCalendar').append(rendered);
-			});
-		}else{
-			setTimeout(() => {
-				htmlRender();
-			}, 1000);
-		}
-	};
+var htmlRender = function() {
+	initDinamicVars();
+	FXStreet.Util.loadHtmlTemplate(htmlTemplateFile).done(function(template) {
+		var rendered = FXStreet.Util.renderByHtmlTemplate(template, dinamicVars);
+		$('#icoCalendar').append(rendered);
+	});
+};
 
-	htmlRender();
+htmlRender();
