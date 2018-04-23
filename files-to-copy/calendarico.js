4203cd90986200525a1424806de0430206c1758d
Oriol Pérez <oriol@fxstreet.com>
2018-04-23 11:39:22 +0200

    Update calendarico.js

--- files-to-copy/calendarico.js
+++ files-to-copy/calendarico.js
@@ -1,5 +1,5 @@
-var view = {
-        name : "Joe",
-        occupation : "Web Developer"
-      };
-
+$("#templates").load("template.html #template1",function(){
+          var template = document.getElementById('template1').innerHTML;
+          var output = Mustache.render(template, view);
+          $("#person").html(output);
+        });
