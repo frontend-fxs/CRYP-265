03912352cef3927f8fd7c16b6d29efcb74d37378
frontend-fxs <oriol@fxstreet.com>
2018-04-23 15:01:01 +0200

    update

---
+++ copy-content-by-order/b-translations.js
@@ -0,0 +1,16 @@
+var translations =  {
+		Active: 'ACTIVE',
+		Upcoming: 'UPCOMING',
+        Active: 'ACTIVE',
+        ListYourIco: 'List your ICO',
+        Ico: 'ICO',
+        TimeLeft: 'TIME LEFT',
+        Window: 'WINDOW',
+        Description: 'DESCRIPTION',
+        Platinum: 'PLATINUM',
+        Gold:'GOLD',
+        Silver: 'SILVER',
+        Bronze: 'BRONZE',
+        Website: 'Website',
+        Report: 'Report'
+};
--- copy-content-by-order/c-calendarico.js
+++ copy-content-by-order/c-calendarico.js
@@ -1,5 +1,7 @@
-$("#templates").load("copy-content-by-order/d-mustache.html #template1",function(){
-          var template = document.getElementById('template1').innerHTML;
-          var output = Mustache.render(template, view);
-          $("#person").html(output);
-        });
+var dinamicVars = {};
+
+$(document).load('copy-content-by-order/d-mustache.html #template1', function() {
+	var template = document.getElementById('template1').innerHTML;
+	var output = Mustache.render(template, dinamicVars);
+	$('#icoCalendar').html(output);
+});
