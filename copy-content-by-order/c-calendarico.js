67b47a896533d0d982293cbc9e32030bad4785a7
frontend-fxs <oriol@fxstreet.com>
2018-04-23 15:20:27 +0200

    update

--- copy-content-by-order/c-calendarico.js
+++ copy-content-by-order/c-calendarico.js
@@ -1,6 +1,11 @@
 var dinamicVars = {};
 
-$(document).load('copy-content-by-order/d-mustache.html #template1', function() {
+var initDinamicVars = function() {
+	dinamicVars.Translations = translations;
+};
+
+$('#target').load('copy-content-by-order/d-mustache.html #template1', function() {
+	initDinamicVars();
 	var template = document.getElementById('template1').innerHTML;
 	var output = Mustache.render(template, dinamicVars);
 	$('#icoCalendar').html(output);
