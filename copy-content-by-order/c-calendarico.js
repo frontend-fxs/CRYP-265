42e7153a1a4b9fe5bc6b6e583bd9395501bf5096
frontend-fxs <oriol@fxstreet.com>
2018-04-24 11:20:10 +0200

    update

--- copy-content-by-order/c-calendarico.js
+++ copy-content-by-order/c-calendarico.js
@@ -1,14 +1,47 @@
 var dinamicVars = {};
 var htmlTemplateFile = 'copy-content-by-order/d-mustache.html';
+
+var IsEndedIco = function(ico) {
+	var IsEnded = ico.endDate.getTime() < dinamicVars.Now.getTime();
+	return IsEnded;
+};
+
+var IsStartedIco = function(ico) {
+	var isStarted = ico.startDate.getTime() < dinamicVars.Now.getTime();
+	return IsStarted;
+};
+
+var getActiveIcos = function() {
+	dinamicVars.Icos.Active = icos.filter(ico => !IsEndedIco(ico) && IsStartedIco(ico));
+};
+
+var getUpcomingIcos = function() {
+	dinamicVars.Icos.Upcoming = icos.filter(ico => !IsEndedIco(ico) && !IsStartedIco(ico));
+};
+
+var getRecentIcos = function() {
+	dinamicVars.Icos.Upcoming = icos.filter(ico => IsEndedIco(ico) && IsStartedIco(ico));
+};
+
+var filterIcosByDate = function() {
+	getActiveIcos();
+	getUpcomingIcos();
+	getRecentIcos();
+};
+
+var updateDinamicVars = function() {
+	dinamicVars.Now = new Date();
+	filterIcosByDate();
+};
+
 var initDinamicVars = function() {
 	dinamicVars.Translations = translations;
+	updateDinamicVars();
 };
 
 var htmlRender = function() {
 	initDinamicVars();
-	
-	
-	$('#target').load('copy-content-by-order/d-mustache.html', function() {
+	$('#target').load(htmlTemplateFile, function() {
 		var template = document.getElementById('icoCalendarTemplate').innerHTML;
 		var output = Mustache.render(template, dinamicVars);
 		$('#icoCalendar').html(output);
@@ -16,3 +49,7 @@ var htmlRender = function() {
 };
 
 htmlRender();
+
+setInterval(function() {
+	htmlRender();
+}, 60000);
