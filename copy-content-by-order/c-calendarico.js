0d442c6de5e9bf95e17ba5edb8bcaff5d18e68b3
frontend-fxs <oriol@fxstreet.com>
2018-04-24 11:22:38 +0200

    update

--- copy-content-by-order/c-calendarico.js
+++ copy-content-by-order/c-calendarico.js
@@ -2,13 +2,13 @@ var dinamicVars = {};
 var htmlTemplateFile = 'copy-content-by-order/d-mustache.html';
 
 var IsEndedIco = function(ico) {
-	var IsEnded = ico.endDate.getTime() < dinamicVars.Now.getTime();
-	return IsEnded;
+	var isEnded = ico.endDate.getTime() < dinamicVars.Now.getTime();
+	return isEnded;
 };
 
 var IsStartedIco = function(ico) {
 	var isStarted = ico.startDate.getTime() < dinamicVars.Now.getTime();
-	return IsStarted;
+	return isStarted;
 };
 
 var getActiveIcos = function() {
